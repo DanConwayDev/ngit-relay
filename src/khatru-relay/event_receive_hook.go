2d27293cf5d7cdb40c8867e71c9fb27a7cfe33b4
DanConwayDev <DanConwayDev@protonmail.com>
2025-05-08 17:17:45 +0100

    feat: initialize repo when event received
    create the git repo when a state or announcement event is received

---
+++ src/khatru-relay/event_receive_hook.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
+
+func EventRecieveHook(git_data_path string) func(ctx context.Context, event *nostr.Event) {
+	return func(ctx context.Context, event *nostr.Event) {
+		if event.Kind == nostr.KindRepositoryAnnouncement || event.Kind == nostr.KindRepositoryState {
+			npub, _ := nip19.EncodePublicKey(event.PubKey)
+			identifier := event.Tags.Find("d")[1]
+			repo_path := git_data_path + "/" + npub + "/" + identifier + ".git"
+
+			if _, err := os.Stat(repo_path); err == nil {
+				// repo directory exists
+			} else if !os.IsNotExist(err) {
+				fmt.Println("Error checking repo path:", err)
+			} else {
+				// repo doesn't exist
+				fmt.Println("Creating empty git repo for ", npub+"/"+identifier)
+
+				// create directory
+				err := os.MkdirAll(repo_path, os.ModePerm)
+				if err != nil {
+					fmt.Println("Error creating directory:", err)
+					return
+				}
+				fmt.Println("Directory created successfully:", repo_path)
+
+				// init git repo
+				cmd := exec.Command("git", "init", "--bare", repo_path)
+				cmd.Dir = git_data_path // Set the working directory for the command
+				_, err = cmd.Output()
+				if err != nil {
+					fmt.Println("Error initializing Git repository:", err)
+					return
+				}
+
+				// set permissions
+				err = os.Chmod(repo_path, 0777)
+				if err != nil {
+					fmt.Println("Error changing permissions:", err)
+					return
+				}
+
+				// allow unauthenticated push (we handle write permissions via pre-receive git hook)
+				cmd = exec.Command("git", "config", "http.receivepack", "true")
+				cmd.Dir = repo_path // Set the working directory for the command
+				_, err = cmd.Output()
+				if err != nil {
+					fmt.Println("Error configuring Git to enable push:", err)
+					return
+				}
+
+				fmt.Println("Created empty git repo")
+			}
+		}
+	}
+}
--- src/khatru-relay/main.go
+++ src/khatru-relay/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/fiatjaf/eventstore/badger"
@@ -12,6 +12,29 @@ import (
 )
 
 func main() {
+
+	type Config struct {
+		relay_data_path string
+		git_data_path   string
+	}
+	// Define flags for relay-data-dir and git-data-dir
+	relay_data_path := flag.String("relay-data-dir", "", "Directory for relay data")
+	git_data_path := flag.String("git-data-dir", "", "Directory for repositories data")
+
+	// Parse the flags
+	flag.Parse()
+
+	// Check if the required arguments are provided
+	if *relay_data_path == "" || *git_data_path == "" {
+		fmt.Println("Both relay-data-dir and git-data-dir are required.")
+		flag.Usage()
+		return
+	}
+	config := Config{
+		relay_data_path: *relay_data_path, // Dereference the pointer to get the string value
+		git_data_path:   *git_data_path,   // Dereference the pointer to get the string value
+	}
+
 	// Create new relay
 	relay := khatru.NewRelay()
 
@@ -21,9 +44,9 @@ func main() {
 	relay.Info.Description = "Nostr relay powered by Khatru"
 	relay.Info.Icon = ""
 
-	path := os.Args[1]
-	db := badger.BadgerBackend{Path: path}
+	db := badger.BadgerBackend{Path: config.relay_data_path}
 	db.Init()
+	relay.OnEventSaved = append(relay.OnEventSaved, EventRecieveHook(config.git_data_path))
 	relay.StoreEvent = append(relay.StoreEvent, db.SaveEvent)
 	relay.QueryEvents = append(relay.QueryEvents, db.QueryEvents)
 	relay.CountEvents = append(relay.CountEvents, db.CountEvents)
