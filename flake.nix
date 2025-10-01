fc85c6ba59a0e0961ece79bf6475acdcfad89b4f
DanConwayDev <DanConwayDev@protonmail.com>
2025-10-01 12:15:12 +0100

    remove nix module and build
    this experiment didnt work. we would have to maintain
    both a dockerfile and a nix version

--- flake.nix
+++ flake.nix
@@ -1,43 +1,17 @@
 {
   description =
-    "ngit-relay flake: builds image and provides NixOS service module";
+    "ngit-relay - a nostr-permissiond git server with embedded relay";
 
   inputs = {
+    # Use the latest stable version of Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        dockerTools = pkgs.dockerTools;
-        image = dockerTools.buildImage {
-          name = "ngit-relay-image";
-          tag = "latest";
-          created = "now";
-          copyToRoot = pkgs.buildEnv {
-            name = "ngit-relay-root";
-            paths = [ dockerTools.binSh dockerTools.caCertificates ];
-          };
-          config = {
-            Cmd = [ "/bin/sh" ];
-            WorkingDir = "/app";
-          };
-        };
-
-        nixosModule = import ./modules/ngit-relay-module.nix;
-      in {
-        packages = {
-          image = image;
-          default = image;
-        };
-
-        defaultPackage = image;
-
-        devShells.default = pkgs.mkShell { buildInputs = [ pkgs.go ]; };
-      }) // {
-        # Export module at top level, outside of eachDefaultSystem
-        nixosModules.ngit-relay = import ./modules/ngit-relay-module.nix;
+  outputs = { self, nixpkgs }: {
+    # Define the development shell
+    devShells.x86_64-linux.default =
+      nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.go ];
       };
+  };
 }
